067fb3591342199416c80c6566888f2dfe06735d
osmiisslow <osmi@goober.cloud>
2025-04-12 20:07:16 -0500

    kde support? :3

---
+++ modules/nixos/desktop/kde/default.nix
@@ -0,0 +1,25 @@
+{
+  inputs,
+  lib,
+  config,
+  pkgs,
+  ...
+}: {
+  services.xserver = {
+    enable = true;
+    libinput.enable = true;
+    displayManager.sddm = {
+      package = pkgs.kdePackages.sddm;
+      wayland = true;
+      enable = true;
+    };
+    desktopManager.plasma6.enable = true;
+  };
+  environment.systemPackages = with pkgs; [
+    kitty # terminal replace
+    vlc # we should probably have this .w.
+  ];
+  environment.plasma6.excludePackages = with pkgs.kdePackages; [
+    konsole
+  ];
+}
